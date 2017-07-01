16064be3d643ed4bf2779037ba64124f640af650
James Cuzella <james.cuzella@lyraphase.com>
2017-07-01 02:21:03 -0600

    Add test-kitchen + ServerSpec tests for: rhel, fedora

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -3,14 +3,15 @@ require 'spec_helper'
 describe 'hfsplus::default' do
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
-  packages = ['hfsplus', 'hfsutils', 'hfsprogs', 'gdisk']
+  if ['debian', 'ubuntu'].include?(os[:family])
+    packages = ['hfsplus', 'hfsutils', 'hfsprogs', 'gdisk']
+  elsif os[:family] == 'redhat'
+    packages = ['kmod-hfsplus', 'hfsplus-tools', 'kmod-hfs', 'gdisk']
+  elsif os[:family] == 'fedora'
+    packages = ['hfsplus-tools',  'gdisk']
+  end
 
-  [
-    'hfsplus',
-    'hfsutils',
-    'hfsprogs',
-    'gdisk'
-  ].each do |pkg|
+  packages.each do |pkg|
     describe package(pkg) do
       it { should be_installed }
     end
